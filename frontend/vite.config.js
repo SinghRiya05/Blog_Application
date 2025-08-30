68033ff0d985781b9755a8dea75c1fb330a8b0a7
SinghRiya05 <riyasingh969641@gmail.com>
2025-08-30 15:36:59 +0530

    refactor: Remove server proxy configuration from Vite setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:"./",
-  server: {
-    proxy: {
-      '/blog': {
-        target: 'https://blog-application-94u9.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''), // "/api/blog/AllBlog" -> "/blog/AllBlog"
-      },
-    },
-  },
+  base: './',   // production ke liye yahi rakho
 })
