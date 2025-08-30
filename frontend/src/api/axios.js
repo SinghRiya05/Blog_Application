71b5b5ca99fc8396285535ed804e0c84a76d707b
SinghRiya05 <riyasingh969641@gmail.com>
2025-08-30 21:39:30 +0530

    done

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({  
   // baseURL: "https://blog-application-94u9.onrender.com/",
-  baseURL:"https://blog-application-94u9.onrender.com",
+  baseURL:"https://blog-application-94u9.onrender.com/",
   withCredentials: true,              
 });
 api.interceptors.request.use((config) => {
